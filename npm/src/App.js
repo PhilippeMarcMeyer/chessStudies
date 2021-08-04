c39b61a62360f8ae48cfc071e01c88de2ea77791
Philippe MEYER <p.meyer@mappingcontrol.com>
2021-08-04 22:26:39 +0200

    first commit

---
+++ npm/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-title">
+          Chess Studies
+        </div>
+      </header>
+      <game>
+        
+      </game>
+    </div>
+  );
+}
+
+export default App;
