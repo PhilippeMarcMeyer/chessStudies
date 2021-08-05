cffd3f6cce6cd85f0424e3c792ba2c8649f58859
Philippe MEYER <p.meyer@mappingcontrol.com>
2021-08-05 20:46:04 +0200

    setting the state for a new game

--- src/components/board/board.component.jsx
+++ src/components/board/board.component.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import './board.styles.css';
 
-class Board extends React.Component {
-
-    renderSquare(i) {
-     // return <Square />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
+const Board = (data) => {
       return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
    
           </div>
@@ -24,6 +15,5 @@ class Board extends React.Component {
           </div>
         </div>
       );
-    }
   }
   export default Board;
---
+++ src/components/game/game.component.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import './game.styles.css';
+import Board from '../board/board.component';
+
+class Game extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+              "infos":"New Game",
+              "msg":"White to play",
+              "data":[],
+              "move":{"number":0,"side":"w"},
+              "positions":[],
+              "columns" : "a,b,c,d,e,f,g,h".split(","),
+              "rows" : "1,2,3,4,5,6,7,8".split(","),
+              "figures" : "R,K,B,Q,K,B,K,R".split(","),
+              "pawn" : "p",
+              "black": "B",
+              "white": "W",
+              "blackColor" : "#000",
+              "whiteColor" : "#fff"
+        };
+      }
+      setNewGame = () => {
+        let data = [];
+        let positions =  [];
+        let blackColor = this.state.blackColor;
+        let whiteColor = this.state.whiteColor;
+        let black =  this.state.black;
+        let white =  this.state.white;
+
+        let currentColor = blackColor;
+        let i = 0;
+        this.state.columns.forEach((col) =>{
+          this.state.rows.forEach((row) =>{
+            i++;
+            currentColor = i % 2 === 1 ? blackColor : whiteColor;
+            data.push({"row":row,"column":col,"squareColor":currentColor,"mob":null,"side":null});
+          })
+        })
+  
+        this.state.figures.forEach((fig,i) =>{
+          positions.push({"row":this.state.rows[0],"column":this.state.columns[i],"mob":fig,"side":white});
+          positions.push({"row":this.state.rows[1],"column":this.state.columns[i],"mob":this.state.pawn,"side":white});
+          positions.push({"row":this.state.rows[6],"column":this.state.columns[i],"mob":this.state.pawn,"side":black});
+          positions.push({"row":this.state.rows[7],"column":this.state.columns[i],"mob":fig,"side":white});
+        });
+
+        data.forEach((sqr)=>{
+            let check = positions.filter((pos)=>{
+              return pos.row === sqr.row && pos.column === sqr.column;
+            });
+            if(check.length===1){
+              sqr.mob = check[0].mob;
+              sqr.side = check[0].side;
+            }
+        });
+        this.setState((prevState,prevProps) => {
+          return {
+            "data": data,
+            "positions":positions,
+            "move":{"number":0,"side":"w"}
+           }
+        });
+      }
+
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board key={1} data={this.state} />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  export default Game;
+
